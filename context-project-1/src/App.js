8262380f2ebeceb241e2175d38514b127b266da3
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-06 21:10:51 +0530

    movie list added

---
+++ context-project-1/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import MovieList from "./components/MovieList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Context Api tutorial</h1>
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
---
+++ context-project-1/src/components/Nav.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nav = () => {
+  return <div>Number of movies :</div>;
+};
+
+export default Nav;
