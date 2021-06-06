73bd919fa4d6bdd60cc661be720f9111f93bc277
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-06 23:00:31 +0530

    useReducer

--- context-project-1/src/App1.jsx
+++ context-project-1/src/App1.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { createContext } from "react";
 import CompC from "./context/CompC";
+import Counter1 from "./context/Counter1";
 
-export const UserContext = createContext();
-export const TitleContext = createContext();
+// export const UserContext = createContext();
+// export const TitleContext = createContext();
 
 const App1 = () => {
   return (
-    <UserContext.Provider value={"Debasis "}>
-      <TitleContext.Provider value={"Saikia"}>
-        <CompC />
-      </TitleContext.Provider>
-    </UserContext.Provider>
+    // <UserContext.Provider value={"Debasis "}>
+    //   <TitleContext.Provider value={"Saikia"}>
+    //     <CompC />
+    //   </TitleContext.Provider>
+    // </UserContext.Provider>
+    <Counter1 />
   );
 };
 
--- context-project-1/src/context/Counter1.jsx
+++ context-project-1/src/context/Counter1.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 const Counter1 = () => {
-  return <div></div>;
+  const [count, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div>
+      <h2>Count - {count}</h2>
+      <button onClick={() => dispatch("increment")}>Increment</button>
+      <button onClick={() => dispatch("decrement")}>Decrement</button>
+      <button onClick={() => dispatch("reset")}>Reset</button>
+    </div>
+  );
 };
 
 export default Counter1;
